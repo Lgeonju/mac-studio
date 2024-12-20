939ea93cc17cb7dde642ed1161df3f59926516a5
LeeGeonju <dlrjswn8747@naver.com>
2024-12-20 23:10:07 +0900

    메인 작업중

---
+++ src/fragment/Header.js
@@ -0,0 +1,21 @@
+import {Link} from 'react-router-dom';
+import '../scss/header.scss'
+
+function Header() {
+    return (
+        <header>
+            <div className="headerInner">
+                <h1><Link to="/">Mac Studio</Link></h1>
+
+                <nav>
+                    <ul>
+                        <li><Link to="/">개요</Link></li>
+                        <li><Link to="/">제품사양</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>    
+    );
+}
+
+export default Header;
