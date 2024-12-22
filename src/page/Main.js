57258971864087066b7bd29c5f283687bcb548bc
LeeGeonju <dlrjswn8747@naver.com>
2024-12-22 23:57:41 +0900

    히어로 섹션 작업중

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,3 +1,4 @@
+import MainSection1 from "./mainSectionPage/MainSection1";
 import MainSection2 from "./mainSectionPage/MainSection2";
 import MainSection3 from "./mainSectionPage/MainSection3";
 import MainSection4 from "./mainSectionPage/MainSection4";
@@ -7,6 +8,7 @@ import MainSection6 from "./mainSectionPage/MainSection6";
 function Main() {
   return (
     <>
+      <MainSection1/>
       <MainSection2/>
       <MainSection3/>
       <MainSection4/>
---
+++ src/page/mainSectionPage/MainSection1.js
@@ -0,0 +1,84 @@
+import "../../scss/main.scss";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger) //플러그인 꼭 연결해줘야함 안하면 스크롤 트리거가 작동이 안됨
+
+function MainSection1(){
+
+    useGSAP(()=>{
+
+        const images = document.querySelectorAll('.imgAni > img');
+  
+        let imagesAni = gsap.timeline({
+            scrollTrigger: {
+            trigger: ".mainSection1",
+            start: "top top",
+            end:"bottom top",
+            pin:true,
+            pinSpacing:true,
+            markers:false, //가상으로 스크롤이벤트 범위를 보여줌
+            scrub:true,
+            }
+        });
+        imagesAni.to(images, {opacity:1, stagger:43, duration: 0.2});
+    });
+
+    return(
+        <section className="mainSection1">
+            <div className="sectionContent">
+                <div className="introCopy">
+                    <p className="s">Mac Studio</p>
+                    <p className="m">강력함의 집약체.</p>
+                </div>
+                <div className="imgAni">
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0000.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0001.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0002.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0003.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0004.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0005.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0006.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0007.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0008.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0009.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0010.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0011.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0012.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0013.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0014.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0015.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0016.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0017.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0018.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0019.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0020.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0021.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0022.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0023.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0024.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0025.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0026.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0027.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0028.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0029.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0030.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0031.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0032.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0033.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0034.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0035.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0036.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0037.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0038.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0039.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0040.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0041.jpg`} alt=""/>
+                    <img src={`${process.env.PUBLIC_URL}/images/hero/0042.jpg`} alt=""/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MainSection1;
